ffce46f790b3bcf7b05779481f3335c4458ecfca
Matheus O.G. do Nascimento <82417324+Matheus-O-G-do-Nascimento@users.noreply.github.com>
2021-10-14 15:10:47 -0300

    Add files via upload

---
+++ Rental/BD/senai-rental-03-DQL.sql
@@ -0,0 +1,17 @@
+USE M_Rental;
+GO
+
+SELECT * FROM Empresa
+ORDER BY idEmpresa
+
+SELECT * FROM Cliente
+ORDER BY idCliente
+
+SELECT * FROM Marca
+ORDER BY idMarca
+
+SELECT * FROM Modelo
+
+SELECT * FROM Veiculo
+
+SELECT * FROM Aluguel
