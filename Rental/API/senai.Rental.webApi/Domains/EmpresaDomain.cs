4c67404a0d916574f27936e915ddb3c1e260cfe7
Matheus O.G. do Nascimento <82417324+Matheus-O-G-do-Nascimento@users.noreply.github.com>
2021-11-02 19:32:34 -0300

    Add files via upload

---
+++ Rental/API/senai.Rental.webApi/Domains/EmpresaDomain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rental.Domains
+{
+    public class EmpresaDomain
+    {
+
+        public int idEmpresa { get; set; }
+
+        public string nomeEmpresa { get; set; }
+    }
+}
---
+++ Rental/API/senai.Rental.webApi/Domains/VeiculoDomain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rental.Domains
+{
+    public class VeiculoDomain
+    {
+        
+        public int idVeiculo { get; set; }
+
+        private int placaVeiculo { get; set; }
+
+
+        public int idModelo { get; set; }
+
+        public int idMarca { get; set; }
+
+        public int idEmpresa { get; set; }
+    }
+}
