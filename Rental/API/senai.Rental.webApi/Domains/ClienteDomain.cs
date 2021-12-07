07e7e557512acc03a998a435a457e982c7a7ce31
Matheus O.G. do Nascimento <82417324+Matheus-O-G-do-Nascimento@users.noreply.github.com>
2021-12-07 11:19:08 -0300

    Add files via upload

---
+++ Rental/API/senai.Rental.webApi/Domains/ClienteDomain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rental.Domains
+{
+    public class ClienteDomain
+    {
+
+        public int idCliente { get; set; }
+
+        public string nomeCliente { get; set; }
+
+        private string cpfCliente { get; set; }
+    }
+}
