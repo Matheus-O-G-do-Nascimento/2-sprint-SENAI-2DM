21753ff57371dc310475241568b76e5aff8add5f
Matheus O.G. do Nascimento <82417324+Matheus-O-G-do-Nascimento@users.noreply.github.com>
2021-12-08 11:24:07 -0300

    Add files via upload

---
+++ Rental/API/senai.Rental.webApi/Interfaces/IAluguelRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Rental.Domains;
+using Rental.Repositories;
+
+namespace Rental.Interfaces
+{
+    interface IAluguelRepository
+    {
+        public List<AluguelDomain> Listar();
+
+        public AluguelDomain BuscarPorId(int id);
+
+        public void Inserir(AluguelDomain aluguel);
+
+        public void Deletar(int id);
+
+        public void Atualizar(int id, AluguelDomain aluguelAtualizado);
+    }
+}
